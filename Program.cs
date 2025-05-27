328294f45f1df26407ac82ee9edd02c6507d89e8
rahmaAlmamari <alr816125@gmail.com>
2025-05-27 11:25:33 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SP_Trigger_Challenge_TeamAmaniMathlaRahma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
